7555021a1aeebf599013fd1934c907c2c09a1453
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2005-11-09 18:28:10 +0000

    * lib/: Utilisation maintenant de ce rep pour mettre le code utilis� par les deux programmes.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@25 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ lib/Outils.cpp
@@ -0,0 +1,47 @@
+/* src/Outils.cpp
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * $Id$
+ */
+
+#include <string>
+
+#include "Outils.h"
+
+std::string stringtok(std::string &in, const char * const delimiters = " \t\n")
+{
+	std::string::size_type i = 0;
+	std::string s;
+
+		// eat leading whitespace
+		i = in.find_first_not_of (delimiters, i);
+		if (i == std::string::npos)
+		{
+			s = in;
+			in = "";
+			return s;   // nothing left but white space
+		}
+
+		// find the end of the token
+		std::string::size_type j = in.find_first_of (delimiters, i);
+
+		// push token
+			s = in.substr(i, j-i);
+			in = in.substr(j+1);
+
+		return s;
+}
