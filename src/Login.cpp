86054f69fbef1632f4cb7183bb206072a15de7e3
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2005-11-11 20:23:11 +0000

    * src/Login.cpp: Rajout du fichier avec les fonctions de logging. 	(HELCommand::Exec): Fonction HELLO 	(PIGCommand::Exec): Fonction PING 	(POGCommand::Exec): Fonction PONG
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@29 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ src/Login.cpp
@@ -0,0 +1,36 @@
+/* src/Login.cpp - Login commands
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * $Id$
+ */
+
+#include "Commands.h"
+#include "Sockets.h"
+
+/* HEL <prog> <version> */
+int HELCommand::Exec(EC_Client *me, CL_Array<CL_String> parv)
+{
+	me->send(me->rpl(EC_Client::IAM), me->app->getconf()->nick.get_string());
+	return 0;
+}
+
+int PIGCommand::Exec(EC_Client *me, CL_Array<CL_String> parv)
+{
+	me->send(me->rpl(EC_Client::PONG));
+
+	return 0;
+}
