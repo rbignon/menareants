84839589dbdd46ae90a427a2ed377e520a7c2e2c
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2005-11-11 20:22:34 +0000

    * server/Commands.h, server/Server.c: Parsage des commandes correct.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@28 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ server/Commands.h
@@ -0,0 +1,62 @@
+/* src/Commands.h - Command list
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * $Id$
+ */
+
+#ifndef ECD_COMMANDS_H
+#define ECD_COMMANDS_H
+
+#include <string>
+#include <vector>
+#include "Main.h"
+#include "Server.h"
+
+class EC_ACommand
+{
+friend class ECServer;
+public:
+	EC_ACommand(const std::string _CmdName, unsigned short _flags, unsigned short _args)
+		: CmdName(_CmdName), flags(_flags), args(_args)
+	{}
+
+	virtual ~EC_ACommand() {}
+
+	virtual int Exec(TClient *cl, std::vector<std::string> string_list) = 0;
+
+private:
+	std::string CmdName;
+	unsigned short flags;
+	unsigned short args;
+};
+
+#define DECLARE_CMD(commName) \
+class commName##Command : public EC_ACommand \
+{ \
+public: \
+	commName##Command(const std::string _CmdName, unsigned short _flags, unsigned short _args) \
+		: EC_ACommand(_CmdName, _flags, _args) \
+	{} \
+	virtual ~commName##Command() {} \
+	virtual int Exec(TClient *cl, std::vector<std::string> string_list); \
+}
+
+DECLARE_CMD ( IAM );
+DECLARE_CMD ( PIG );
+DECLARE_CMD ( POG );
+
+#endif
