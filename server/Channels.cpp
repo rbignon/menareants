2d8508be9f77d86d6f23f78a7896c251ec1a8d68
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2005-11-22 19:45:36 +0000

    (EChannels): Structure serveur des channels. (ECPlayer): Structure serveur des players.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@67 6832cf7c-ce15-0410-9f4e-c008ec2580bf

--- server/Channels.cpp
+++ server/Channels.cpp
@@ -15,9 +15,80 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  * $Id$
  */
 
+#include "../lib/Channels.h"
 #include "Channels.h"
+#include "Server.h"
 
-std::vector<EChannels*> ChanList;
+std::vector<EChannel*> ChanList;
+
+/********************************************************************************************
+ *                              Commandes                                                   *
+ ********************************************************************************************/
+
+/* JOI <nom> */
+int JOICommand::Exec(TClient *cl, std::vector<std::string> parv)
+{
+	std::string nom = parv[1];
+	EChannel* chan;
+	ECPlayer* pl;
+	unsigned int i;
+
+	for(i=0; i<ChanList.size() && (ChanList[i]->GetName() != nom || !(chan = ChanList[i];i++);
+
+	if(!chan)
+	{ /* Création du salon */
+		chan = new EChannel(nom);
+		pl = new ECPlayer(cl, chan);
+		chan->AddPlayer(pl);
+		cl->sendrpl(app.rpl(ECServer::JOIN), cl->nick, nom, "");
+		cl->sendrpl(app.rpl(ECServer::SETS), "");
+		cl->sendrpl(app.rpl(ECServer::PLIST), );
+	}
+	else
+	{ /* Rejoins un salon existant */
+
+	}
+	return 0;
+}
+
+/********************************************************************************************
+ *                               EPlayer                                                    *
+ ********************************************************************************************/
+
+ECPlayer::ECPlayer(TClient *_client, EChannel *_chan)
+	: ECBPlayer(_chan), client(_client)
+{
+
+}
+
+/********************************************************************************************
+ *                               EChannel                                                   *
+ ********************************************************************************************/
+
+void EChannel::NeedReady()
+{
+	for(unsigned int i=0;i<players.size();i++)
+		players[i]->Ready = false;
+	return;
+}
+
+ECPlayer *EChannel::GetPlayer(char* nick)
+{
+	for(unsigned int i=0;i<players.size();i++)
+		if(players[i]->Client() &&
+		   !strcasecmp(players[i]->Client()->GetNick(), nick))
+			return players[i];
+	return NULL;
+}
+
+ECPlayer *EChannel::GetPlayer(TClient *cl)
+{
+	for(unsigned int i=0;i<players.size();i++)
+		if(players[i]->Client() == cl)
+			return players[i];
+	return NULL;
+}
--- server/Channels.h
+++ server/Channels.h
@@ -15,14 +15,83 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  * $Id$
  */
 
 #ifndef ECD_CHANNELS_H
 #define ECD_CHANNELS_H
 
-#include "../lib/Channels.h"
+class TClient;
 
-extern std::vector<EChannels*> ChanList;
+class ECBPlayer;
+
+class ECBChannel;
+
+class EChannel;
+
+/********************************************************************************************
+ *                               ECPlayers                                                  *
+ ********************************************************************************************/
+class ECPlayer : public ECBPlayer
+{
+/* Constructeurs/Deconstructeurs */
+public:
+	ECPlayer(TClient* cl, EChannel* chan);
+
+/* Methodes */
+public:
+
+/* Attributs */
+public:
+
+	/* Salon auquel appartient le player */
+	EChannel *Channel() { return chan; }
+
+	/* Obtient le client */
+	TClient *Client() { return client; }
+
+	/* Le joueur est pret (utilisation de RDY) */
+	bool Ready;
+
+/* Variables privées */
+protected:
+	TClient *client;
+	EChannel *chan;
+};
+
+/********************************************************************************************
+ *                               EChannel                                                   *
+ ********************************************************************************************/
+
+class EChannel : public ECBChannel
+{
+/* Constructeurs/Deconstructeurs */
+public:
+
+/* Methodes */
+public:
+
+	/* Nécessite que tous les joueurs soient prets */
+	void NeedReady();
+
+/* Attributs */
+public:
+
+	/* Récupère la liste des joueurs */
+	std::vector<ECPlayer*> Players() { return players; }
+
+	/* Récupère le Player par le pseudo */
+	ECPlayer* GetPlayer(char* nick);
+
+	/* Récupère le Player par sa classe cliente */
+	ECPlayer *EChannel::GetPlayer(TClient *cl);
+
+/* Variables privées */
+protected:
+	std::vector<ECPlayer*> players;
+};
+
+extern std::vector<EChannel*> ChanList;
 
 #endif /* ECLIB_CHANNELS_H */
