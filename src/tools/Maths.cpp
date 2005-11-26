e015447aac32717913c2eac9455ba740a8108f18
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2005-11-26 16:35:18 +0000

    * Cr�ation fichier avec deux petites fonctions.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@88 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ src/tools/Maths.cpp
@@ -0,0 +1,23 @@
+#include <math.h>
+#include "Maths.h"
+
+// Modèle pour borner une valeur entre min et max
+template <class T>
+T BorneTpl (const T &valeur, const T &min, const T &max)
+{
+  if (valeur < min)
+    return min;
+  else if (max < valeur)
+    return max;
+  else
+    return valeur;
+}
+
+//-----------------------------------------------------------------------------
+
+long BorneLong (const long &valeur, const long &min, const long &max)
+{ return BorneTpl (valeur, min, max); }
+
+double BorneDouble (const double &valeur, const double &min, const double &max)
+{ return BorneTpl (valeur, min, max); }
+
---
+++ src/tools/Maths.h
@@ -0,0 +1,3 @@
+long BorneLong (const long &valeur, const long &min, const long &max);
+double BorneDouble (const double &valeur, const double &min,
+                    const double &max);
