c905ecda1183ac9af06936f7484ef8a170b40c8e
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2005-11-27 16:57:29 +0000

    * src/gui/BouttonText.cpp: Rajout du fichier avec ButtonText pour dessiner un bouton (�wormux)
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@97 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ src/gui/BouttonText.cpp
@@ -0,0 +1,72 @@
+/* src/gui/BouttonText.cpp - Boutton Text
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ */
+
+#include "BouttonText.h"
+
+ButtonText::ButtonText() : Button()
+{
+  font = NULL;
+}
+
+//-----------------------------------------------------------------------------
+
+ButtonText::ButtonText (unsigned int x, unsigned int y, unsigned int w, unsigned int h, const std::string &text)
+  : Button(x, y, w, h)
+{
+  m_text = text;
+}
+
+
+//-----------------------------------------------------------------------------
+
+void ButtonText::SetFont (Font *font)
+{
+  this->font = font;
+}
+
+//-----------------------------------------------------------------------------
+
+void ButtonText::SetText(const std::string &text)
+{
+  m_text = text;
+}
+
+//-----------------------------------------------------------------------------
+
+void ButtonText::Draw (unsigned int souris_x, unsigned int souris_y)
+{
+  if(image)
+    DrawImage (souris_x, souris_y);
+
+  assert (font != NULL);
+
+  const int x = GetX()+GetWidth()/2;
+  const int y = GetY()+GetHeight()/2;
+
+  font->WriteCenter (x, y, m_text, enabled ? white_color : gray_color);
+}
+
+//-----------------------------------------------------------------------------
+
+std::string ButtonText::GetText() const
+{
+  return m_text;
+}
---
+++ src/gui/BouttonText.h
@@ -0,0 +1,47 @@
+/* src/gui/BouttonText.h - Header of BouttonText.cpp
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ */
+
+#ifndef EC_BOUTTONTEXT_H
+#define EC_BOUTTONTEXT_H
+
+#include "Boutton.h"
+#include "../tools/Font.h"
+#include <string>
+
+class ButtonText : public Button
+{
+private:
+  std::string m_text;
+
+  Font *font;
+
+public:
+  ButtonText();
+  ButtonText (unsigned int x, unsigned int y, unsigned int w, unsigned int h, const std::string &text);
+
+  virtual void Draw (unsigned int souris_x, unsigned int souris_y);
+  void SetText (const std::string &text);
+  void SetFont (Font *font);
+
+  std::string GetText() const;
+};
+
+#endif /* EC_BOUTTONTEXT_H */
