0a05a893b6dbce98e20f44010a7751a3f7062fcc
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-02-17 16:07:28 +0000

    * src/Map.cpp: Cr�ation des diff�rentes classes ainsi que de diverses fonctions. Le chargement de la map MARCHE !
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@229 6832cf7c-ce15-0410-9f4e-c008ec2580bf

--- lib/Map.cpp
+++ lib/Map.cpp
@@ -18,5 +18,379 @@
  *
  * $Id$
  */
- 
+
 #include "Map.h"
+#include "Debug.h"
+#include "Outils.h"
+#include <fstream>
+
+/********************************************************************************************
+ *                               ECBCase                                                    *
+ ********************************************************************************************/
+
+ECBCase::ECBCase(ECBMap* _map, uint _x, uint _y, uint _flags, char _type_id)
+	: map(_map),
+	  x(_x),
+	  y(_y),
+	  flags(_flags),
+	  type_id(_type_id)
+{
+	map_country = 0;
+}
+
+/********************************************************************************************
+ *                               ECBCountry                                                 *
+ ********************************************************************************************/
+
+ECBCountry::ECBCountry(char _ident[3])
+{
+	strcpy(ident, _ident);
+	owner = 0;
+}
+
+/********************************************************************************************
+ *                               ECBMapPlayer                                               *
+ ********************************************************************************************/
+
+ECBCountry* ECBMapPlayer::FindCountry(const char* c)
+{
+	for(std::vector<ECBCountry*>::iterator it=countries.begin(); it != countries.end(); ++it)
+		if(!strcmp(c, (*it)->ID()))
+			return *it;
+	return NULL;
+}
+
+/********************************************************************************************
+ *                               ECBMap                                                     *
+ ********************************************************************************************/
+
+template<typename T>
+static ECBCase* CreateCase(ECBMap *map, uint x, uint y, uint flags, char type_id)
+{
+	return new T(map, x, y, flags, type_id);
+}
+
+static struct
+{
+	char c;
+	ECBCase* (*func) (ECBMap *map, uint x, uint y, uint flgs, char type_id);
+	uint flags;
+} case_type[] = {
+	{ 'v', CreateCase<ECBVille>, C_VILLE            },
+	{ 'V', CreateCase<ECBVille>, C_VILLE|C_CAPITALE },
+	{ 'm', CreateCase<ECBMer>,   C_MER              },
+	{ 't', CreateCase<ECBTerre>, C_TERRE            },
+	{ 'p', CreateCase<ECBPont>,  C_PONT             }
+};
+
+ECBMap::ECBMap(std::vector<std::string> _map_file)
+{
+	initialised = false;
+
+	map_file = _map_file;
+	
+	Init();
+}
+
+ECBMap::ECBMap(std::string filename)
+{
+	initialised = false;
+
+	std::ifstream fp(filename.c_str());
+
+	if(!fp)
+		throw ECExcept(VName(filename), "Impossible d'ouvrir le fichier");
+
+	std::string ligne;
+
+	while(std::getline(fp, ligne))
+		map_file.push_back(ligne);
+
+	Init();
+}
+
+/** \page Map_Format Map's file format
+ *
+ * A map is defined by a file.
+ *
+ * This will respect this architecture :
+ *
+ * <pre>
+ *   NAME [name]
+ *   PLAYER [identificator]
+ *   X [x]
+ *   Y [y]
+ *   MAP
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   WXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZWXXYZ
+ *   EOM
+ *   BEGIN [begining money]
+ *   CITY [money by city]
+ *   MIN [min]
+ *   MAX [max]
+ * </pre>
+ *
+ *  1) NAME [name]
+ *   <pre>
+ *   A map have a name. So put it here.
+ *   </pre>
+ *  2) PLAYER [identificator]
+ *   <pre>
+ *   It's a player. Identificator is an unik number ('A' to 'Z').
+ *   </pre>
+ *  3) X [x]
+ *   <pre>
+ *   This is the maximal number of horizontal cases.
+ *   </pre>
+ *  4) Y [y]
+ *   <pre>
+ *   This is the maximal number of vertical cases.
+ *   </pre>
+ *  5) MAP
+ *   <pre>
+ *   This is the body of map. After this message, and until have a "EOM" message,
+ *   there is map body where identificators. All cases are identified like this :
+ *
+ *                       WXXYZ
+ *
+ *   W = Type of terrain (a city, sea)
+ *      'v' = ville.
+ *      'V' = ville | capitale.
+ *      'm' = mer.
+ *      't' = terre.
+ *      'p' = pont.
+ *   XX = Identificator of country (like AA, AB, etc)
+ *   Y = Player Owner (like A, B, C, etc, or neutral)
+ *   Z = Type of architecture (only for image, so not used by server)
+ *   </pre>
+ *
+ *  6) BEGIN [begining money]
+ *   <pre>
+ *   Here we define money that all players have when they begin game.
+ *   </pre>
+ *
+ *  7) CITY [money by city]
+ *   <pre>
+ *   Here we put money win by each city.
+ *   </pre>
+ *
+ *  8) MIN [min]
+ *   <pre>
+ *   Min players.
+ *   </pre>
+ *
+ *  6) MAX [max]
+ *   <pre>
+ *   Max players. It's channel limit.
+ *   </pre>
+ *
+ * @author Progs
+ * @date 15/02/2006
+ */
+void ECBMap::Init()
+{
+	chan = 0;
+	x = 0;
+	y = 0;
+	min = 0;
+	max = 0;
+	
+	std::string ligne;
+
+	bool recv_map = false;
+	uint _x = 0, _y = 0;
+
+	for(std::vector<std::string>::iterator it = map_file.begin(); it != map_file.end(); ++it)
+	{
+		ligne = (*it);
+		if(ligne[0] == '#' || ligne[0] == '\0') continue; /* Commentaire */
+		if(ligne == "EOM")
+		{
+			if(_y < y)
+				throw ECExcept(VIName(_y) VIName(y), "Il n'y a pas assez de lignes !");
+			recv_map = false;
+			continue;
+		}
+		if(!recv_map)
+		{
+			std::string key = stringtok(ligne, " ");
+	
+			if(key == "NAME") name = ligne;
+			else if(key == "X") x = atoi(ligne.c_str());
+			else if(key == "Y") y = atoi(ligne.c_str());
+			else if(key == "PLAYER")
+				map_players.push_back(new ECBMapPlayer(ligne[0]));
+			else if(key == "MAP") recv_map = true;
+			else if(key == "BEGIN") begin_money = atoi(ligne.c_str());
+			else if(key == "CITY") city_money = atoi(ligne.c_str());
+			else if(key == "MIN") min = atoi(ligne.c_str());
+			else if(key == "MAX") max = atoi(ligne.c_str());
+			else
+				throw ECExcept(VName(key) VName(name), "Fichier map incorrect");
+		}
+		else
+		{
+			if(!x || !y)
+				throw ECExcept(VIName(x) VIName(y) VName(name), "Fichier map incorrect");
+			if(map_players.empty())
+				throw ECExcept(VIName(map_players.size()), "Aucun player !?");
+			if(_y >= y)
+				throw ECExcept(VIName(_y) VIName(y), "Trop de cases");
+			uint size = ligne.size();
+			_x = 0;
+			uint counter = 0;
+			ECBCase *acase = 0;
+			ECBCountry *country = 0;
+			char c_id[3];
+			for(uint i=0; i<size; ++i)
+			{
+				if(_x >= x)
+						throw ECExcept(VIName(_x) VIName(x), "Trop de cases");
+
+				/* WXXYZ
+				 * W(terrain), XX(country), Y(owner), Z(architecture)
+				 */
+				switch(counter)
+				{
+					/* Type du terrain (utilisation de case_type */
+					case 0:
+					{
+						for(uint j=0; j < (sizeof case_type / sizeof *case_type); j++)
+							if(case_type[j].c == ligne[i])
+							{
+								acase = case_type[j].func (this, _x, _y, case_type[j].flags, case_type[j].c);
+								break;
+							}
+						if(!acase)
+							throw ECExcept(VPName(acase) VCName(ligne[i]), "Terrain introuvable");
+						map.push_back(acase);
+						break;
+					}
+					/* Récupération de l'identification de la Country (première partie) */
+					case 1:
+					{
+						c_id[0] = ligne[i];
+						c_id[1] = '\0', c_id[2] = '\0';
+						break;
+					}
+					/* Identification de la Country */
+					case 2:
+					{
+						c_id[1] = ligne[i];
+						std::vector<ECBCountry*>::iterator it;
+						for(it = map_countries.begin();
+						    it != map_countries.end() && strcmp(c_id, (*it)->ID()); ++it);
+						if(it == map_countries.end())
+						{ /* La country n'existe pas encore */
+							country = new ECBCountry(c_id);
+							map_countries.push_back(country);
+						}
+						else /* La country existe déjà */
+							country = *it;
+						acase->SetCountry(country);
+						country->AddCase(acase);
+						break;
+					}
+					/* Player owner (utilisation de map_players) */
+					case 3:
+					{
+						std::vector<ECBMapPlayer*>::iterator it;
+						for(it=map_players.begin();
+						    it != map_players.end() && (*it)->ID() != ligne[i]; it++);
+						if(it == map_players.end())
+							throw ECExcept(VCName(ligne[i]) VIName(map_players.size()), "Player introuvable !?");
+
+						if(!country->Owner())
+							country->SetOwner(*it);
+						else if(country->Owner() != *it)
+							throw ECExcept(VCName(country->Owner()->ID()) VCName((*it)->ID())
+							               VIName(_x) VIName(_y), "Une country a deux owners !");
+
+						if((*it)->FindCountry(c_id)) /* Ce MapPlayer a déjà cette country en mémoire */
+							break;
+						(*it)->AddCountry(country);
+						break;
+					}
+					/* Type d'architecture
+					 * TODO: renvoie sur une fonction virtuelle
+					 */
+					case 4:
+					{
+						break;
+					}
+					default:
+						throw ECExcept(VIName(counter), "Compteur anormal !?");
+				}
+
+				if(counter >= 4)
+				{
+					_x++;
+					acase = 0;
+					counter = 0;
+				}
+				else counter++;
+			}
+			if(_x < x)
+				throw ECExcept(VIName(_y) VIName(_x) VIName(x), "Il n'y a pas assez de case sur cette ligne");
+			_y++;
+		}
+	}
+	/* Vérification finale des données */
+	if(!begin_money || !city_money || !x || !y || map.empty() || map_players.empty() || map_countries.empty() ||
+	   !min || !max || map_players.size() != max || min > max)
+		throw ECExcept(VIName(begin_money) VIName(city_money) VIName(x) VIName(y) VIName(map.size()) VIName(min) VIName(max)
+		               VIName(map_players.size()) VIName(map_countries.size()), "Fichier incorrect !");
+
+	/* On vérifie si il y a bien une ville par country */
+	for(std::vector<ECBCountry*>::iterator it= map_countries.begin(); it != map_countries.end(); ++it)
+	{
+		uint count = 0;
+		std::vector<ECBCase*> acase = (*it)->Cases();
+		std::vector<ECBCase*>::iterator cc;
+		for(cc = acase.begin(); cc != acase.end(); ++cc)
+			if((*cc)->Flags() & C_VILLE)
+				count++;
+		if(count != 1)
+			throw ECExcept(VIName(count) VSName((*it)->ID()),
+			               count > 1 ? "La country a trop de villes !" : "La country n'a pas de ville !");
+	}
+
+	/* La map est *bien* initialisée !! */
+	initialised = true;
+}
+
+ECBMap::~ECBMap()
+{
+	/* Libération des cases */
+	for(std::vector<ECBCase*>::iterator it=map.begin(); it != map.end(); ++it)
+		delete *it;
+
+	/* Libération des MapPlayers */
+	for(std::vector<ECBMapPlayer*>::iterator it=map_players.begin(); it != map_players.end(); ++it)
+		delete *it;
+		
+	/* Libération des Countries */
+	for(std::vector<ECBCountry*>::iterator it=map_countries.begin(); it != map_countries.end(); ++it)
+		delete *it;
+}
+
+ECBCase* ECBMap::operator() (uint _x, uint _y) const
+{
+	if(!initialised)
+		throw ECExcept(0, "ECBMap n'est pas initialisé");
+
+	if (_x >= x || _y >= y)
+		throw ECExcept(VIName(x) VIName(y) VIName(_x) VIName(_y), "Access à un element hors du tableau");
+
+	/** \warning ALIGNEMENT FAIT LIGNE PAR LIGNE !!! */
+	return map[ _y * x + _x ];
+}
--- lib/Map.h
+++ lib/Map.h
@@ -23,16 +23,137 @@
 #define ECLIB_MAP_H
 
 #include "Channels.h"
+#include <string>
 #include <vector>
 
+/** @page Map_structure Map structure
+ *
+ * All data are organized like this :
+ * <pre>
+ * ECBMap
+ *  |- std::vector<ECBCases*>  <-----------
+ *  |- std::vector<ECBCountry*>  <----     \
+ *  `- std::vector<ECBMapPlayer*>     \    |
+ *      `- std::vector<ECBCountry*> ---    /
+ *          `- std::vector<ECBCases*> -----
+ * </pre>
+ */
+
+class ECBMap;
+
+class ECBMapPlayer;
+
+class ECBCountry;
+
 /********************************************************************************************
- *                               ECBPlayer                                                  *
+ *                               ECBCase                                                    *
  ********************************************************************************************/
 
+#define C_VILLE          0x0001
+#define C_CAPITALE       0x0002
+#define C_TERRE          0x0004
+#define C_MER            0x0008
+#define C_PONT           0x0010
+
+/** This is a Case's class...
+ */
 class ECBCase
 {
 /* Constructeur/Destructeur */
 public:
+
+	ECBCase(ECBMap* _map, uint _x, uint _y, uint _flags, char _type_id);
+
+/* Methodes */
+public:
+
+/* Attributs */
+public:
+
+	/** This case is a member of this map */
+	ECBMap* Map() { return map; }
+
+	char TypeID() { return type_id; }
+
+	uint Flags() { return flags; }
+
+	ECBCountry* Country() { return map_country; }
+	void SetCountry(ECBCountry *mc) { map_country = mc; }
+
+/* Variables privées */
+protected:
+
+	ECBMap *map;
+
+	uint x, y;
+
+	uint flags;
+
+	char type_id;
+
+	ECBCountry *map_country;
+};
+
+/** This class is a derived class from ECBCase whose is a city */
+class ECBVille : public ECBCase
+{
+/* Constructeur/Destructeur */
+public:
+	ECBVille(ECBMap* _map, uint _x, uint _y, uint _flags, char _type_id) : ECBCase(_map, _x, _y, _flags, _type_id) {}
+
+/* Methodes */
+public:
+
+/* Attributs */
+public:
+
+/* Variables privées */
+protected:
+
+};
+
+/** This class is a derived class from ECBCase whose is a land */
+class ECBTerre : public ECBCase
+{
+/* Constructeur/Destructeur */
+public:
+	ECBTerre(ECBMap* _map, uint _x, uint _y, uint _flags, char _type_id) : ECBCase(_map, _x, _y, _flags, _type_id) {}
+
+/* Methodes */
+public:
+
+/* Attributs */
+public:
+
+/* Variables privées */
+protected:
+
+};
+
+/** This class is a derived class from ECBCase whose is sea */
+class ECBMer : public ECBCase
+{
+/* Constructeur/Destructeur */
+public:
+	ECBMer(ECBMap* _map, uint _x, uint _y, uint _flags, char _type_id) : ECBCase(_map, _x, _y, _flags, _type_id) {}
+
+/* Methodes */
+public:
+
+/* Attributs */
+public:
+
+/* Variables privées */
+protected:
+
+};
+
+/** This class is a derived class from ECBCase whose is a bridge */
+class ECBPont : public ECBCase
+{
+/* Constructeur/Destructeur */
+public:
+	ECBPont(ECBMap* _map, uint _x, uint _y, uint _flags, char _type_id) : ECBCase(_map, _x, _y, _flags, _type_id) {}
 
 /* Methodes */
 public:
@@ -46,22 +167,95 @@ protected:
 };
 
 /********************************************************************************************
+ *                               ECBCountry                                                 *
+ ********************************************************************************************/
+/** This class is a country in the map
+ *
+ * \warning \a cases is a vector of pointers to ECBMap::map, so we don't have to delete them.
+ */
+class ECBCountry
+{
+/* Constructeur/Destructeur */
+public:
+
+	ECBCountry(char _ident[3]);
+
+/* Attributs */
+public:
+
+	void AddCase(ECBCase* _case) { cases.push_back(_case); }
+	
+	const char* ID() { return ident; }
+
+	ECBMapPlayer* Owner() { return owner; }
+	void SetOwner(ECBMapPlayer* mp) { owner = mp; }
+
+	std::vector<ECBCase*> Cases() { return cases; }
+
+/* Variables privées */
+protected:
+	std::vector<ECBCase*> cases;
+	ECBMapPlayer *owner;
+	char ident[3];
+};
+
+/********************************************************************************************
+ *                               ECBMapPlayer                                               *
+ ********************************************************************************************/
+/** This is a Map Player to identify him on the map
+ *
+ * \warning \a countries is a vector of pointers to ECBMap::map_countries, so we don't have to delete them.
+ */
+class ECBMapPlayer
+{
+/* Constructeur/Destructeur*/
+public:
+
+	ECBMapPlayer(char _id)
+		: id(_id)
+	{}
+
+/* Attributs */
+public:
+
+	char ID() { return id; }
+
+	void AddCountry(ECBCountry* _country) { countries.push_back(_country); }
+
+	ECBCountry* FindCountry(const char*);
+
+	std::vector<ECBCountry*> Countries() { return countries; }
+
+/* Variables privées */
+public:
+	char id;
+	std::vector<ECBCountry*> countries;
+
+};
+
+/********************************************************************************************
  *                               ECBMap                                                     *
  ********************************************************************************************/
 
+/** This is a base of Map class.
+ *
+ * \warning there is a lot of exceptions in constructor ! Si you *must* use a try/except block when you
+ *          create an ECBMap !
+ */
 class ECBMap
 {
 /* Constructeur/Destructeur */
 public:
 
-	/** Constructor
-	 * @param _name name of map
-	 * @param min min players to play
-	 * @param max max players to play
-	 * @param x x cases
-	 * @param y y cases
+	/** Path of map (used only by Server) */
+	ECBMap(std::string _filename);
+
+	/** Constructor from a string's vector
+	 * @param _map_file this is a string's vector where there is informations about map
 	 */
-	ECBMap(std::string _name, uint min, uint max, uint x, uint y);
+	ECBMap(std::vector<std::string> _map_file);
+
+	~ECBMap();
 
 /* Methodes */
 public:
@@ -74,14 +268,50 @@ public:
 
 	/** Channel linked to map */
 	ECBChannel* Channel() { return chan; }
+	
+	uint BeginMoney() { return begin_money; }          /**< All players have this money when they begin the game */
+	uint CityMoney() { return city_money; }            /**< This is money that is given to players at each turn by cities */
+	uint MinPlayers() { return min; }                  /**< Min players to play */
+	uint MaxPlayers() { return max; }                  /**< Max players to play */
+	uint NbCases() { return map.size(); }              /**< Number of cases */
+	uint NbMapPlayers() { return map_players.size(); } /**< Number of map players */
+	uint NbCountries() { return map_countries.size(); }/**< Number of map countries */
+
+	uint Width() { return x; }
+	uint Height() { return y; }
+
+	std::vector<ECBCase*> Cases() { return map; }                   /**< Return case vector */
+	std::vector<ECBMapPlayer*> MapPlayers() { return map_players; } /**< Return map players vector */
+	std::vector<ECBCountry*> Countries() { return map_countries; }  /**< Return countries vector */
+	
+	/** Access to a case of map 
+	 * Example: map(x,y)
+	 */
+	ECBCase* operator() (uint x, uint y) const;
 
 /* Variables privées */
 protected:
-	ECBCase **map;
+	std::vector<ECBCase*> map;              /**< Alignement fait ligne par ligne !!! */
+
+	std::vector<std::string> map_file;
+	
+	std::vector<ECBMapPlayer*> map_players;
+	
+	std::vector<ECBCountry*> map_countries;
+
 	ECBChannel *chan;
 	std::string name;
+
 	uint x, y;
 	uint min, max;
+
+	uint begin_money, city_money;
+
+	bool initialised; /**< This variable is setted to true only when \a map is empty */
+
+	void Init();      /**< Initialisation, called by constructors */
+
+	ECBMap();         /**< Disallowed */
 };
 
 #endif /* ECLIB_MAP_H */
