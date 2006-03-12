8b1ac8873247e642f2d7a92037c252f4e146e2d8
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-03-12 18:35:46 +0000

    * server/InGame.cpp: Gestion des cr�ations et am�lioration des animations.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@348 6832cf7c-ce15-0410-9f4e-c008ec2580bf

--- server/InGame.cpp
+++ server/InGame.cpp
@@ -24,13 +24,14 @@
 #include "Debug.h"
 #include "Outils.h"
 #include "Commands.h"
+#include "Units.h"
 
 void EChannel::InitAnims()
 {
 
 }
 
-#define SHOW_EVENT(x) ((x) & ARM_NUMBER ? "number" : (x) & ARM_MOVE ? "move" : (x) & ARM_ATTAQ ? "attaq" : "no")
+#define SHOW_EVENT(x) ((x) == ARM_UNION ? "union" : (x) == ARM_MOVE ? "move" : (x) == ARM_ATTAQ ? "attaq" : (x) == ARM_CREATE ? "create" : (x) == ARM_NUMBER ? "number" : "no")
 void EChannel::NextAnim()
 {
 	if(!map) throw ECExcept(VPName(map), "Pas de map");
@@ -53,6 +54,13 @@ void EChannel::NextAnim()
 		case ARM_UNION:
 			SendArm(NULL, event->Entities()->List(), ARM_MOVE|ARM_REMOVE, event->Case()->X(), event->Case()->Y());
 			break;
+		case ARM_NUMBER:
+			SendArm(NULL, event->Entities()->List(), ARM_NUMBER|ARM_HIDE, 0, 0, event->Nb());
+			break;
+		case ARM_CREATE:
+			SendArm(NULL, event->Entities()->List(), ARM_CREATE|ARM_HIDE, event->Case()->X(), event->Case()->Y(),
+			                                                                       event->Nb(), event->Type());
+			break;
 		case ARM_MOVE:
 			SendArm(NULL, event->Entities()->List(), ARM_MOVE, event->Case()->X(), event->Case()->Y());
 			break;
@@ -71,6 +79,21 @@ void EChannel::NextAnim()
 	dynamic_cast<ECMap*>(map)->RemoveEvent(event, USE_DELETE);
 }
 
+template<typename T>
+static ECEntity* CreateEntity(const Entity_ID _name, ECBPlayer* _owner, ECase* _case, uint _nb)
+{
+	return new T(_name, _owner, _case, _nb);
+}
+
+static struct
+{
+	ECEntity* (*func) (const Entity_ID _name, ECBPlayer* _owner, ECase* _case, uint _nb);
+} entities_type[] = {
+	/* E_ARMY */{ CreateEntity<ECArmy> },
+	/* E_END  */{ NULL }
+};
+
+
 /** Modification of an army.
  *
  * Syntax: ARM nom [+nb] [%type] [/nb] [>pos] [*pos]
@@ -87,18 +110,16 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 	EChannel* chan = cl->Player()->Channel();
 	ECMap *map = dynamic_cast<ECMap*>(chan->Map());
 	ECEntity* entity = 0;
+	uint flags = 0;
 
-	if(parv[1] == "-")
+	if(parv[1] != "-") /* Si parv[1] == "-" alors on créé une nouvelle entity */
 	{
-		/** \todo Création d'une nouvelle entité */
-	}
-	else
 		entity = dynamic_cast<ECEntity*>(cl->Player()->Entities()->Find(parv[1].c_str()));
 
-	if(!entity)
-		return vDebug(W_DESYNCH, "Entité introuvable", VPName(entity) VName(parv[1]));
+		if(!entity)
+			return vDebug(W_DESYNCH, "Entité introuvable", VPName(entity) VName(parv[1]));
+	}
 
-	uint flags = 0;
 	uint y = 0, x = 0, type = 0, nb = 0;
 	ECBCase* last_case = 0;
 	for(uint i = 2; i<parv.size(); i++)
@@ -114,7 +135,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 				x = StrToTyp<uint>(stringtok(s, ","));
 				y = StrToTyp<uint>(s);
 
-				if((last_case = entity->Move(x, y)))
+				if(!entity || (last_case = entity->Move(x, y)))
 					flags |= ARM_MOVE;
 				break;
 			}
@@ -127,21 +148,119 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 				x = StrToTyp<uint>(stringtok(s, ","));
 				y = StrToTyp<uint>(s);
 
-				if((last_case = entity->Attaq(x, y)))
+				if(entity && (last_case = entity->Attaq(x, y)))
 					flags |= ARM_ATTAQ;
 				break;
 			}
 			case '+':
+			{
+				if(!entity)
+				{
+					nb = StrToTyp<uint>(parv[i].substr(1));
+					flags |= ARM_NUMBER;
+				}
+				break;
+			}
 			case '/':
 			case '%':
+			{
+				type = StrToTyp<uint>(parv[i].substr(1));
+				flags |= ARM_TYPE;
+				break;
+			}
 			default: Debug(W_DESYNCH, "ARM: Flag %c non supporté (%s)", parv[i][0], parv[i].c_str());
+		}
+	}
+	if(!entity)
+	{ /* Création d'une entité */
+		/******************************
+		 *    GESTION DES CRÉATIONS   *
+		 ******************************/
+		if(flags != ARM_CREATE || type >= ECBEntity::E_END)
+			return Debug(W_DESYNCH, "ARM: Création d'une entité incorrecte");
+
+		const char *e_name = chan->FindEntityName(cl->Player());
+		entity = entities_type[type].func(e_name, cl->Player(), (*(chan->Map()))(x,y), nb);
+		if(!entity->CanBeCreated())
+		{
+			MyFree(entity);
+			flags = 0;
+		}
+		else
+		{
+			/** \todo: mettre ici concernant l'argent */
+			ECEvent* event = 0;
+
+			if(entity->Case()->Entities()->Sames(entity))
+			{ /* On peut probablement faire une union avec une entité de cette case donc bon au lieu de
+			   * se broyer les testicule on va ARM_NUMBER simplement */
+				EventVector evts = map->Events();
+				bool event_found = false;
+				for(EventVector::iterator evti = evts.begin(); evti != evts.end(); ++evti)
+				{
+					ECEntity* et = *((*evti)->Entities()->List().begin());
+					if((*evti)->Case() == entity->Case() && (*evti)->Flags() == ARM_NUMBER &&
+					   et->Owner() == entity->Owner() && et->Type() == entity->Type())
+					{ /* On a trouvé un ARM_NUMBER qu'on investit */
+						(*evti)->SetNb((*evti)->Nb() + entity->Nb());
+						MyFree(entity);
+						entity = et;
+						entity->AddUnits(nb);
+						event = *evti;
+						Debug(W_DEBUG, "On a trouvé un ancien ARM_NUMBER qu'on investit");
+
+						event_found = true;
+						break;
+					}
+				}
+				if(!event_found)
+				{ /* On créé un nouvel evenement */
+					event = new ECEvent(ARM_NUMBER, entity->Case());
+					std::vector<ECBEntity*> ents = entity->Case()->Entities()->List();
+					ECEntity* my_friend = 0;
+					for(std::vector<ECBEntity*>::iterator enti = ents.begin(); enti != ents.end(); ++enti)
+						if(!(*enti)->Locked() && (*enti)->Owner() == entity->Owner() && (*enti)->Type() == entity->Type())
+							my_friend = dynamic_cast<ECEntity*>(*enti);
+					if(!my_friend)
+						throw ECExcept(VIName(entity->Case()->Entities()->Sames(entity)) VIName(ents.size()),
+									"Il y a comme un problème là");
+	
+					Debug(W_DEBUG, "On rajoute directement les unités dans cette armée.");
+	
+					my_friend->AddUnits(entity->Nb());
+	
+					MyFree(entity);
+					entity = my_friend;
+	
+					event->SetNb(entity->Nb());
+					event->Entities()->Add(entity);
+					map->AddEvent(event);
+				}
+				flags = ARM_NUMBER;
+			}
+			else
+			{
+				Debug(W_DEBUG, "On rajoute un ARM_CREATE");
+				event = new ECEvent(ARM_CREATE, entity->Case());
+				event->Entities()->Add(entity);
+				event->SetNb(entity->Nb());
+				event->SetType(entity->Type());
+				chan->Map()->AddAnEntity(entity);
+				map->AddEvent(event);
+			}
+			nb = event->Nb();
+			entity->SetEvent(flags);
 		}
 	}
 	if(flags)
 	{
 		std::vector<TClient*> recvers;
-		if(flags & ARM_ATTAQ || flags & ARM_MOVE)
+		if(flags == ARM_ATTAQ || flags & ARM_MOVE)
 		{
+			/******************************
+			 *  GESTION DES DÉPLACEMENTS  *
+			 *    ET DES ATTAQUES         *
+			 ******************************/
 			entity->SetEvent(flags);
 
 			EventVector evts = map->Events();
@@ -150,7 +269,9 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 			{
 				Debug(W_DEBUG, "%p(%d,%d) vs %p(%d,%d)", (*evti)->Case(), (*evti)->Case()->X(), (*evti)->Case()->Y(),
 				                                         entity->Case(), entity->Case()->X(), entity->Case()->Y());
-				if((*evti)->Case() == entity->Case())
+
+				if((*evti)->Case() == entity->Case() && ((*evti)->Flags() == ARM_MOVE || (*evti)->Flags() == ARM_ATTAQ) ||
+				                                         (*evti)->Flags() == ARM_UNION)
 				{ /* Si il existe déjà un evenement sur cette case, on rajoute notre entité là. */
 					bool attaq = true;
 					if(flags & ARM_MOVE)
@@ -160,7 +281,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 						std::vector<ECEntity*> ents = (*evti)->Entities()->List();
 						for(ECList<ECEntity*>::iterator enti = ents.begin(); enti != ents.end(); ++enti)
 						{
-							if((*enti)->Locked()) continue;
+							if((*enti)->Locked() || *enti == entity) continue;
 
 							if((*enti)->Owner() == entity->Owner() && (*enti)->Type() == entity->Type())
 							{ /* On va unir les deux armées */
@@ -168,7 +289,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 								{ /* Cet evenement est une union donc on se propose de rajouter
 								   * notre unité ici.
 								   */
-									(*enti)->SetEvent(ARM_UNION);
+									(*enti)->SetEvent(flags == ARM_CREATE ? ARM_CREATE : ARM_UNION);
 									(*evti)->Entities()->Add(entity);
 									Debug(W_DEBUG, "On Union dans cet event");
 								}
@@ -193,7 +314,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 								attaq = true;
 						}
 					}
-					if(attaq && !event_found && ((*evti)->Flags() == ARM_MOVE || (*evti)->Flags() & ARM_ATTAQ))
+					if(attaq && !event_found && (*evti)->Flags() & (ARM_MOVE|ARM_ATTAQ))
 					{ /* Ce mouvement se transforme en attaque */
 						std::vector<ECEntity*> etlist = (*evti)->Entities()->List();
 						for(ECList<ECEntity*>::iterator it = etlist.begin(); it != etlist.end(); ++it)
@@ -226,7 +347,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 							enti = it;
 						}
 						/* J'avais déjà fait un deplacement (ici donc), et là j'en refais un donc on m'enlève d'ici */
-						else if(*enti == entity)
+						else if(((*evti)->Flags() == ARM_ATTAQ || (*evti)->Flags() == ARM_MOVE) && *enti == entity)
 						{
 							Debug(W_DEBUG, "ça disparait !");
 							ECList<ECEntity*>::iterator it = enti;
@@ -240,7 +361,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 							if((*enti)->EventType() & ARM_ATTAQ) someone_want_attaq = true;
 							++enti;
 						}
-					if(!someone_want_attaq)
+					if(!someone_want_attaq && (*evti)->Flags() == ARM_ATTAQ)
 						(*evti)->SetFlags(ARM_MOVE);
 					if((*evti)->Entities()->empty() || no_body)
 					{
@@ -254,12 +375,12 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 				}
 				else ++evti;
 			}
-			if(!event_found)
+			if(!event_found && flags != ARM_CREATE)
 			{
 				/* Aucun évenement n'a été trouvé, et donc on créé un. */
 				ECEvent* event;
 
-				if(entity->Case()->Entities()->Available() <= 1)
+				if(!entity->Case()->Entities()->Sames(entity) && !entity->Case()->Entities()->Enemies(entity))
 					event = new ECEvent(ARM_MOVE, entity->Case()),
 					Debug(W_DEBUG, "On créé l'evenement MOVE (%d)", map->Events().size());
 				else
@@ -283,7 +404,7 @@ int ARMCommand::Exec(TClient *cl, std::vector<std::string> parv)
 					}
 					if(my_friend)
 					{ /* Sur la case il y a des unités amies */
-						event->SetFlags(ARM_UNION);
+						event->SetFlags(flags == ARM_CREATE ? ARM_CREATE : ARM_UNION);
 						Debug(W_DEBUG, "On Union dans cet event (%d)", map->Events().size());
 						my_friend->Union(entity); /* entity s'unie dans my_friend */
 					}
