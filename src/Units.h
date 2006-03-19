17c45a5f79f7f0f829b8408c224ab4d07d6f24a0
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-03-19 00:58:49 +0000

    * src/Map.cpp, src/Units.h: D�rivation des cases et entit�es avec leur image, fonctions pour afficher, g�rer, etc.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@391 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ src/Units.h
@@ -0,0 +1,47 @@
+/* src/Units.h - Header of Units.cpp
+ *
+ * Copyright (C) 2005-2006 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ */
+
+#ifndef EC_UNITS_H
+#define EC_UNITS_H
+
+#include "src/Map.h"
+#include "lib/Units.h"
+#include "Resources.h"
+
+class ECArmy : public ECEntity, public ECBArmy
+{
+/* Constructeur/Destructeur */
+public:
+	ECArmy(const Entity_ID _name, ECBPlayer* _owner, ECBCase* _case, uint _nb = ARMY_NB)
+		: ECBEntity(_name, _owner, _case, E_ARMY, ARMY_STEP, ARMY_COST, _nb)
+	{ SetImage(Resources::Army_Face()); }
+
+/* Methodes */
+public:
+
+/* Attributs */
+public:
+
+/* Variables protégées */
+protected:
+};
+
+#endif /* EC_UNITS_H */
