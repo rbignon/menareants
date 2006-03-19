a10d5081a6907f5e251f104ba14e273aa3716fc0
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-03-19 00:51:11 +0000

    * src/gui/ChildForm.h (TChildForm): Composant qui est une Form enfant.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@380 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ src/gui/ChildForm.cpp
@@ -0,0 +1,54 @@
+/* src/gui/ChildForm.cpp - This is a form include in another form
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * $Id$
+ */
+
+#include "ChildForm.h"
+#include "Main.h"
+
+TChildForm::TChildForm(int _x, int _y, uint _w, uint _h)
+	: TComponent(_x, _y, _w, _h)
+{
+	background = NULL;
+}
+
+void TChildForm::SetBackground(ECImage *image)
+{
+	background = image;
+}
+
+void TChildForm::Draw(int _x, int _y)
+{
+	if(background)
+	{
+		SDL_Rect r_back = {x,y,w,h};
+		SDL_BlitSurface(background->Img,NULL,app.sdlwindow,&r_back);
+	}
+
+	for(std::vector<TComponent*>::iterator it = composants.begin(); it != composants.end(); ++it)
+		if((*it)->Visible())
+			(*it)->Draw(_x, _y);
+}
+
+void TChildForm::SetXY(int _x, int _y)
+{
+	for(std::vector<TComponent*>::iterator it =composants.begin(); it != composants.end(); ++it)
+		(*it)->SetXY(((*it)->X() - x) + _x, ((*it)->Y() - y) + _y);
+	x = _x;
+	y = _y;
+}
---
+++ src/gui/ChildForm.h
@@ -0,0 +1,71 @@
+/* src/gui/ChildForm.h - Header of ChildForm.cpp
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * $Id$
+ */
+
+#ifndef EC_CHILDFORM_H
+#define EC_CHILDFORM_H
+
+#include "Component.h"
+#include "tools/Images.h"
+
+class TChildForm : public TComponent
+{
+/* Constructeur/Destructeur */
+public:
+
+	TChildForm(int _x, int _y, uint _w, uint _h);
+	virtual ~TChildForm() {}
+
+/* Methodes */
+public:
+
+	/* Dessine chaques composants */
+	void Draw(int x, int y);
+
+	void Init() {}
+
+/* Attributs */
+public:
+
+	/** Set background picture */
+	void SetBackground(ECImage *image);
+
+	virtual void SetXY(int x, int y);
+
+/* Variables protégées */
+protected:
+
+	ECImage *background;
+
+	/** Add a component */
+	template<typename T>
+	T* AddComponent(T* comp)
+	{
+		composants.push_back(comp);
+		comp->SetXY(comp->X()+x, comp->Y()+y);
+		comp->Init();
+		return comp;
+	}
+
+/* Variables privées */
+private:
+	std::vector<TComponent*> composants;
+};
+
+#endif /* EC_CHILDFORM_H */
