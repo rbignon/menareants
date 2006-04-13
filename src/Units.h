9158cabc657c7f351aa6df2f4c0f037f03ceb6e5
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-04-13 07:59:33 +0000

    modif mineur
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@518 6832cf7c-ce15-0410-9f4e-c008ec2580bf

--- src/Units.h
+++ src/Units.h
@@ -35,7 +35,7 @@ class ECArmy : public ECEntity, public ECBArmy
 /* Constructeur/Destructeur */
 public:
 
-	ECArmy() {}
+	ECArmy() : ECBEntity(E_ARMY) {}
 
 	ECArmy(const Entity_ID _name, ECBPlayer* _owner, ECBCase* _case, uint _nb = ARMY_NB)
 		: ECBEntity(_name, _owner, _case, E_ARMY, ARMY_STEP, ARMY_COST, _nb)
