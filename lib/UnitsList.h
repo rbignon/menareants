1701819f62e7e07e7d75adeec2d79616d87d7033
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-04-13 07:54:01 +0000

    Liste des unit�s qui permet de n'avoir � mettre � jour qu'ici lors d'ajout d'unit�s.
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@505 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ lib/UnitsList.h
@@ -0,0 +1,13 @@
+#ifdef VECTOR
+#define U_BEGIN
+#define U_DECL(x) entities.push_back(new x);
+#define U_END
+#else
+#define U_BEGIN { 0 },
+#define U_DECL(x) { CreateEntity<x> },
+#define U_END { 0 }
+#endif
+
+U_BEGIN
+U_DECL(ECArmy)
+U_END
