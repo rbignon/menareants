2e999a6a4e0126e11f028ea7cc5ed1ade7054d72
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-04-26 17:28:36 +0000

    mini modif
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@564 6832cf7c-ce15-0410-9f4e-c008ec2580bf

--- lib/Units.h
+++ lib/Units.h
@@ -31,16 +31,17 @@
 #define ARMY_NB                    100
 #define ARMY_COST                  2000
 #define ARMY_EMPTY_CONSTRUCTOR(x)  x() : ECBEntity(E_ARMY, ARMY_COST)
-#define ARMY_CONSTRUCTOR(x)        x(const Entity_ID _name, ECBPlayer* _owner, ECBCase* _case, uint _nb = ARMY_NB) :  ECBEntity(_name, _owner, _case, E_ARMY, ARMY_STEP, ARMY_COST, _nb)
+#define ARMY_CONSTRUCTOR(x)        x(const Entity_ID _name, ECBPlayer* _owner, ECBCase* _case, uint _nb = ARMY_NB) \
+                                     :  ECBEntity(_name, _owner, _case, E_ARMY, ARMY_STEP, ARMY_COST, _nb)
 /** This is a simple army */
 class ECBArmy : public virtual ECBEntity
 {
 /* Constructeur/Destructeur */
 public:
 
-  ARMY_EMPTY_CONSTRUCTOR(ECBArmy) {}
+	ARMY_EMPTY_CONSTRUCTOR(ECBArmy) {}
 
-  ARMY_CONSTRUCTOR(ECBArmy) {}
+	ARMY_CONSTRUCTOR(ECBArmy) {}
 
 	virtual ~ECBArmy() {}
 
