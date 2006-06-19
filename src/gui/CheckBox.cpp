81bb6c6e439fc1d17d11883f9c9933f206819e02
progs <progs@6832cf7c-ce15-0410-9f4e-c008ec2580bf>
2006-06-19 17:31:19 +0000

    Checkbox
    git-svn-id: svn://svn.gna.org/svn/menareants/trunk@771 6832cf7c-ce15-0410-9f4e-c008ec2580bf

---
+++ src/gui/CheckBox.cpp
@@ -0,0 +1,74 @@
+/* src/gui/CheckBox.cpp - ComboBox component.
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id: ComboBox.h 766 2006-06-17 11:41:58Z progs $
+ */
+
+#include "CheckBox.h"
+#include "Resources.h"
+#include "gui/Label.h"
+
+TCheckBox::TCheckBox(Font* f, int _x, int _y, std::string txt, SDL_Color color)
+	: TComponent(_x, _y), text(_x+20, _y, txt, color, f), image(0), checked(false)
+{
+
+}
+
+TCheckBox::~TCheckBox()
+{
+	delete image;
+}
+
+void TCheckBox::Init()
+{
+	SetWidth(text.Width()+20);
+
+	delete image;
+
+	image = new ECSprite(Resources::CheckBox(), Window());
+	SetHeight(image->GetHeight() > int(text.Height()) ? image->GetHeight() : text.Height());
+	image->set(X(), Y() + (text.Height() - image->GetHeight())/2);
+
+	MyComponent(&text);
+}
+
+void TCheckBox::Draw(int _x, int _y)
+{
+	unsigned int frame = Test(_x,_y) ? (checked ? 3 : 1) : (checked ? 2 : 0);
+
+	image->SetFrame(frame);
+	image->draw();
+	text.Draw(_x, _y);
+}
+
+void TCheckBox::SetXY (int _x, int _y)
+{
+	TComponent::SetXY(_x,_y);
+	image->set(_x, _y + (text.Height() - image->GetHeight())/2);
+	text.SetXY(_x+20, _y);
+}
+
+bool TCheckBox::Clic(int _x, int _y)
+{
+	if(!TComponent::Clic(_x, _y))
+		return false;
+
+	checked = !checked;
+
+	return true;
+}
---
+++ src/gui/CheckBox.h
@@ -0,0 +1,62 @@
+/* src/gui/CheckBox.h - Header of CheckBox.cpp
+ *
+ * Copyright (C) 2005 Romain Bignon  <Progs@headfucking.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id: ComboBox.h 766 2006-06-17 11:41:58Z progs $
+ */
+
+#ifndef EC_CHECKBOX_H
+#define EC_CHECKBOX_H
+
+#include "Component.h"
+#include "gui/Label.h"
+#include "tools/Font.h"
+
+class TCheckBox : public TComponent
+{
+/* Constructeur/Destructeurs */
+public:
+
+	TCheckBox(Font* f, int x, int y, std::string txt, SDL_Color = black_color);
+	~TCheckBox();
+
+/* Methodes */
+public:
+
+	/** Initialisation */
+	void Init ();
+
+	/** Affiche */
+	void Draw(int m_x, int m_y);
+	bool Clic(int m_x, int m_y);
+
+/* Attributs */
+public:
+
+	bool Checked() const { return checked; }
+	void Check(bool b = true) { checked = b; }
+
+	void SetXY (int _x, int _y);
+
+/* Variables privées */
+private:
+	TLabel    text;
+	ECSprite* image;
+	bool      checked;
+};
+
+#endif /* EC_CHECKBOX_H */
